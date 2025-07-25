27cce7b951f1300989c1a201ad0e23883cbe9093
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-07-25 08:31:41 +0530

    Add files via upload

---
+++ f3.java
@@ -0,0 +1,12 @@
+public class f3 {
+    public static void main(String arg[])
+    {
+        for(int i=1;i<=50;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
