a70551b7ebe2e75c6de9bb0c2930f6a23b47aae5
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-07-29 10:26:00 +0530

    Add files via upload

---
+++ f7.java
@@ -0,0 +1,13 @@
+package Loops;
+//5 table
+public class f7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i*5);
+		}
+	}
+
+}
