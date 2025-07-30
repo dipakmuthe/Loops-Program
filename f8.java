6a20f2671c1462454fb494c4a39af1fa3f1c3a99
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-07-30 10:04:53 +0530

    Add files via upload

---
+++ f8.java
@@ -0,0 +1,18 @@
+package Loops;
+
+import java.util.*;
+
+public class f8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter The Number to Print table ::");
+		int n=sc.nextInt();
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i*n);
+		}
+	}
+
+}
