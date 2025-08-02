e021122a815b3468ba45b72e40081189943fbb9e
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-08-02 12:30:46 +0530

    Add files via upload

---
+++ f11.java
@@ -0,0 +1,19 @@
+package Loops;
+
+public class f11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Number divisible by 3 and 5 ::");
+		for(int i=1;i<=100;i++)	
+		{
+			if(i%5==0 && i%3==0)
+			{
+				System.out.print(" "+i);
+			}
+		}
+		
+	}
+
+}
