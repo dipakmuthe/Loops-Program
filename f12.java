772103f3fe24585c00d02e012ab8ed0c90a24f7d
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-08-03 11:07:44 +0530

    Add files via upload

---
+++ f12.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class f12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for(int i=1;i<=100;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println("Sum of 1 to 100 is ::"+sum);
+	}
+
+}
