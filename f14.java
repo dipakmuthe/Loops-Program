90236ef712304e128d08bbec71e316ab71ad66ec
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-08-05 08:32:49 +0530

    Add files via upload

---
+++ f14.java
@@ -0,0 +1,20 @@
+package Loops;
+
+public class f14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sum=0;
+		for(int i=1;i<=100;i++)
+		{
+			if(i%2!=0)
+			{
+			sum=sum+i;
+			}
+		}
+		System.out.println("Sum of 1 to 100 ODD no is ::"+sum);
+		
+	}
+
+}
