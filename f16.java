b4496b1a06b008caba1934c0748c4ca81c70e266
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-08-07 09:49:18 +0530

    Add files via upload

---
+++ f16.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class f16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		for(int i=1;i<=20;i++)
+		{
+			System.out.println("Square of "+i+" ="+i*i);
+		}
+	}
+
+}
