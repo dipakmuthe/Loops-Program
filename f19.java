45699ac76b22d3bcc12d4f2f08a682403763289b
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-08-12 10:10:13 +0530

    Add files via upload

---
+++ f19.java
@@ -0,0 +1,19 @@
+package Loops;
+
+//Print numbers from 1 to 50, but skip numbers divisible by 5
+
+public class f19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=50;i++)
+		{
+			if(i%5!=0)
+			{
+				
+				System.out.print(" "+i);
+			}
+		}
+	}
+
+}
