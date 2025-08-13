2102a1ee8c2a4f49663c8d7bae5e28c312d44caa
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-08-13 10:15:56 +0530

    Add files via upload

---
+++ f20.java
@@ -0,0 +1,23 @@
+package Loops;
+
+//Print numbers from 1 to 50, but stop when you reach 25.
+
+public class f20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=50;i++)
+		{
+			if(i!=25)
+			{
+				System.out.println(" "+i);
+			}
+			else
+			{
+				break;
+			}
+		}
+
+	}
+
+}
