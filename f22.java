c19f496a89a2dfffc7928e6fe1fd963c28347196
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-08-16 11:32:40 +0530

    Add files via upload

---
+++ f22.java
@@ -0,0 +1,17 @@
+package Loops;
+
+//Print only composite numbers between 1 and 50.
+
+public class f22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 for(int i=1;i<=50;i++)
+		 {
+			 
+		 }
+		
+	}
+
+}
