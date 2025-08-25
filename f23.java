1ae6312e6da3be55da7d60dd5cd655e1a57e1d15
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-08-25 10:59:52 +0530

    Add files via upload

---
+++ f23.java
@@ -0,0 +1,20 @@
+package Loops;
+
+public class f23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=1,count=0;
+		while(i<=100)
+		{
+			if(i%7==0)
+			{
+				count++;
+			}
+			i++;
+		}
+		System.out.println("The count of number is ::"+count);
+	}
+
+}
