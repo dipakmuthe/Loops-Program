9d58587826f3c976351c226f12f268430136a35f
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-09-09 18:51:48 +0530

    Add files via upload

---
+++ f36.java
@@ -0,0 +1,26 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class f36 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Number :");
+		int n=sc.nextInt();
+		int d=0,c=0;
+		while(n>0)
+		{
+			d=n%10;
+			if(d%2!=0)
+			{
+				c++;
+			}
+			
+			n=n/10;
+		}
+		System.out.println("The Odd COunt of digit is ="+c);
+	}
+
+}
