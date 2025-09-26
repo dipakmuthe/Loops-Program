55305d85153f221993a8f70dc1afeb48068f8a5b
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-09-26 10:21:16 +0530

    Add files via upload

---
+++ f45.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class f45 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		do
+		{
+			System.out.println(" "+i);
+			i++;
+		}while(i<=100);
+	}
+
+}
