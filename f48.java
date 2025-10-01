48a9c587030e1091536284791e01bf05e8cb12ac
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-10-01 11:33:07 +0530

    Add files via upload

---
+++ f48.java
@@ -0,0 +1,20 @@
+package Loops;
+
+public class f48 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		do
+		{
+			if(i%2!=0)
+			{
+				System.out.println(" "+i);
+			}
+			
+			i++;
+		}while(i<=100);
+	}
+
+	}
+
