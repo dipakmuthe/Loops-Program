56f6af70bd3bb8b1bfcc897ec4b928e99728c32d
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-10-06 23:30:55 +0530

    Add files via upload

---
+++ f49.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class f49 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=5;i<=100;i=i+5)
+		{
+			System.out.println(" "+i);
+		}
+	}
+
+}
