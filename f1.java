fff9c02de6e3c668d782e2495ac1a4d6167314b4
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-10-26 15:31:23 +0530

    Update f1.java

--- f1.java
+++ f1.java
@@ -2,8 +2,10 @@ public class f1 {
     public static void main(String[] args) {
         for(int i=1;i<=10;i++)
         {
+            
             System.out.print(i+" ");
 
         }
     }
 }
+
